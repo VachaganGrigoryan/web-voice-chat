e3eec195364436e8c822a8f69ea347d6c6a673a8
Vachagan <grigoryan@vachagan.dev>
2026-03-16 17:47:58 +0400

    feat: add WebAuthn support, update dependencies, and include assetlinks.json for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.blackway.voca',
   appName: 'Voca',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://chat.vachagan.dev',
+    cleartext: false
+  }
 };
 
 export default config;
